f378bd6722419d98d94d857671d9d1d9c20c2f7c
Metis Sotangkur <jack10899@gmail.com>
2020-03-13 22:33:13 +0700

    greedy

--- Minimum number of Coins/Minimum number of Coins/main.cpp
+++ Minimum number of Coins/Minimum number of Coins/main.cpp
@@ -7,9 +7,18 @@
 //
 
 #include <iostream>
-
+#include <vector>
+using namespace std;
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    int money, coin = 0;
+    cin>>money;
+    vector<int> myBank = { 1, 2, 5, 10, 20, 50, 100, 500, 1000};
+    for(int i = myBank.size()-1; i>=0; i--){
+        while(money - myBank[i]>=0){
+            coin++;
+            money -= myBank[i];
+        }
+    }
+    cout<<coin<<endl;
     return 0;
 }
